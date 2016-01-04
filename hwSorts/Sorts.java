0608945987ef66f3da5056c186b433b6958b3ef5
Vandana Agarwala <vandana.agarwala@gmail.com>
2016-01-04 12:25:34 -0500

    added selection sort

---
+++ hwSorts/Sorts.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+public class Sorts{
+	public static void printArray(int[] data){
+		String res = "[";
+		for (int i = 0; i < data.length; i++){
+			res += data[i] + ", ";
+		}
+		System.out.println(res.substring(0, res.length() - 2) + "]");
+	}
+	public static void insertionSort(int[] data){
+		int n = data.length;
+        for (int j = 1; j < n; j++) {
+            int key = data[j];
+            int i = j-1;
+            while ( (i > -1) && ( data [i] > key ) ) {
+                data [i+1] = data [i];
+                i--;
+            }
+            data[i+1] = key;
+        }
+    }
+
+    public static int[] selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++){
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];  
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+        return arr;
+    }
+
+    public static void fillRandom(int[] data){
+        Random rand = new Random();
+        for(int i = 0; i < data.length; i++){
+            data[i] = rand.nextInt();
+        }
+    }
+}
